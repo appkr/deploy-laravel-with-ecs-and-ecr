dafafaf993e40c06a2f673c963de595d1993f110
appkr <juwonkim@me.com>
2017-02-12 13:54:00 +0900

    Health 응답 추가

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,14 @@
 */
 
 Route::get('/', function () {
+    return response()->json([
+        'HOST' => gethostname(),
+        'APP_ENV' => env('APP_ENV', 'local'),
+        'APP_DEBUG' => env('APP_DEBUG', 'true'),
+        'APP_VERSION' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+        'APP_URL' => Request::getHttpHost(),
+        'DB_CONNECTION' => env('DB_CONNECTION', 'mysql'),
+    ]);
+
     return view('welcome');
 });
